0bd6a67404d6ba0c2d1244de9987876a32e6de7c
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-08-27 19:49:56 +0530

    New_File

---
+++ src/Basic/PrintNames.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class PrintNames {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		if(a > b) {
+			System.out.println(a + "is Greater");
+		} else 
+			System.out.println(b + "is Greater");
+
+	}
+
+}
