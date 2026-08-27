e386dbfc06409607cfba32054a5062c9e56d6a2b
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-08-27 19:38:32 +0530

    BasicsOfJava

---
+++ src/Basic/Basics.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("New Begining Starts From today");
+	}
+
+}
