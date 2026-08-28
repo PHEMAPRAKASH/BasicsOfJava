e572e88898dd1b0547565824a790772cece479da
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-08-28 22:47:51 +0530

    New_PrintNumbers

--- src/Basic/PrintNumbers.java
+++ src/Basic/PrintNumbers.java
@@ -1,7 +1,7 @@
 package Basic;
 
 public class PrintNumbers {
-
+ //just prints from 0 to 10 numbers using the for loop
 	public static void main(String[] args) {
 		for(int i = 0; i <= 10; i++) {
 			System.out.println(i);
