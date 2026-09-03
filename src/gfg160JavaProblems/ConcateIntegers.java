4ef3d7f30e93ebd4dc44a20d31b76dfa3f3dac4b
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-09-03 19:55:54 +0530

    New_Returnab

---
+++ src/gfg160JavaProblems/ConcateIntegers.java
@@ -0,0 +1,17 @@
+package gfg160JavaProblems;
+
+import java.lang.*;
+import java.util.*;
+
+public class ConcateIntegers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a + "" + b);
+
+	}
+
+}
