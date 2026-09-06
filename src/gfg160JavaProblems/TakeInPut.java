925f529bff1a9446ec8aa3cfa819735a7b34a71d
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-09-06 22:36:42 +0530

    New_TakeInPut

--- src/gfg160JavaProblems/TakeInPut.java
+++ src/gfg160JavaProblems/TakeInPut.java
@@ -1,9 +1,22 @@
 package gfg160JavaProblems;
 
+import java.util.Scanner;
+
 public class TakeInPut {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int n = sc.nextInt();
+		float f = sc.nextFloat();
+		int ff = (int) f;
+		// To Store floor of float variable f
+
+		// code here
+
+		System.out.println(s);
+		System.out.println(n);
+		System.out.println(ff);
 
 	}
 
