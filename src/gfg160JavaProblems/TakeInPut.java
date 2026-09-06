0dee07844153d6f0bae22d3c78c5a68f96a541a9
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-09-06 22:37:59 +0530

    New_TakeInPut

--- src/gfg160JavaProblems/TakeInPut.java
+++ src/gfg160JavaProblems/TakeInPut.java
@@ -2,6 +2,22 @@ package gfg160JavaProblems;
 
 import java.util.Scanner;
 
+/*You need to perform three separate tasks based on the given input:
+
+String Input and Print: Read a string s (which may contain spaces) and print it as it is.
+Integer Input and Print: Read an integer n and print it without any change.
+Float Input and floor Print: Read a floating-point number as input, take its floor value, and print as an integer.
+Examples:
+
+Input: s = "Hello", n = 20, f = 5.5
+Output: 
+Hello
+20
+5
+Explanation: 
+The string Hello is printed as it is.
+The integer 20 is printed without any change.
+For floating-point number 5.5, its floor value 5 is printed. */
 public class TakeInPut {
 
 	public static void main(String[] args) {
