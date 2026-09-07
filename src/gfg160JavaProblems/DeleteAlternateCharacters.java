db1040764d6a4f2ca876ad394e3464a9fa16ffb8
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-09-07 21:07:01 +0530

    New_Delete_alternate_characters

---
+++ src/gfg160JavaProblems/DeleteAlternateCharacters.java
@@ -0,0 +1,24 @@
+package gfg160JavaProblems;
+
+import java.util.Scanner;
+
+public class DeleteAlternateCharacters {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		DeleteAlternateCharacters obj = new DeleteAlternateCharacters();
+		obj.delAlternate("GEEKS");
+		}
+
+	public static String delAlternate(String s) {
+		int n = s.length();
+		String str = "";
+		for (int i = 0; i <= n; i++) {
+			if (n % 2 == 0) {
+				str = str + s.charAt(i);
+			}
+		}
+		return str;
+	}
+}
