7f6f740637574e195d9a0493ddbae187844fb46b
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-09-08 22:28:17 +0530

    New_Commit

--- src/gfg160JavaProblems/DeleteAlternateCharacters.java
+++ src/gfg160JavaProblems/DeleteAlternateCharacters.java
@@ -15,7 +15,7 @@ public class DeleteAlternateCharacters {
 		int n = s.length();
 		String str = "";
 		for (int i = 0; i <= s.length(); i++) {
-			if (i % 2 == 0) {
+			if (i % 2 != 0) {
 				str = str + s.charAt(i);
 			}
 		}
