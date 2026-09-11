83f2cebfc993f9871c6e1f099071e0903aa09a1d
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-09-11 06:34:10 +0530

    New_Commmit

--- src/CarRentalSystem/Car.java
+++ src/CarRentalSystem/Car.java
@@ -1,6 +1,7 @@
 package CarRentalSystem;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Car {
 
@@ -106,11 +107,40 @@ class CarrentalSystem {
 	private List<Car> cars;
 	private List<Customer> customers;
 	private List<Rental> rental;
-	
+
 	public void addcar(Car car) {
 		cars.add(car);
 	}
-	public void caddCustomers(Customer customer) {
+
+	public void addCustomers(Customer customer) {
 		customers.add(customer);
+	}
+
+	public void returncar(Car car, Customer customer, int days) {
+		if (car.isAvailable()) {
+			rental.add(new Rental(car, customer, days));
+		} else {
+			System.out.println("Car is not available for rent.");
+		}
+	}
+	public void returnCar(Car car) {
+		car.retrunCar();
+		Rental rentalToRemove = null;
+		for(Rental rental : rental) {
+			if(rental.getCar() == car) {
+				rentalToRemove = rental;
+				break;
+			}
+			if(rentalToRemove != null) {
+				rental.remove(rentalToRemove);
+				System.out.println("Car returned successfully");
+			}else {
+				System.out.println("Car was not remnted.");
+			}
+		}
+		
+	}
+	public void menu() {
+		Scanner sc = new Scanner(System.in);
 	}
 }
