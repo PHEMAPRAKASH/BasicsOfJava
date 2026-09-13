21f355bc2f10a0db00dcd78e308cf9a93682e72b
HEMAPRAKASH <pphemaprakash@gmail.com>
2026-09-13 18:19:57 +0530

    Commits

--- src/gfg160JavaProblems/ConcatinateAndReverseString.java
+++ src/gfg160JavaProblems/ConcatinateAndReverseString.java
@@ -1,6 +1,21 @@
 package gfg160JavaProblems;
 
 import java.util.Scanner;
+/* Given two strings s1 and s2 as input, concatenate two strings and then reverse the joined string. 
+
+Examples:
+
+Input: s1 = "Geeks" , s2 = "forGeeks"
+Output: "skeeGrofskeeG" 
+Explanation: Concatenating s1 and s2 to get "GeeksforGeeks" then reversing it to "skeeGrofskeeG".
+Input: s1 = "Practice" , s2 = "Geeks"
+Output: "skeeGecitcarP"
+Explanation: Concatenating s1 and s2 to get "PracticeGeeks" then reversing it to"skeeGecitcarP".
+Constraints:
+1 ≤ |s1| ≤103
+1 ≤ |s2| ≤103
+
+*/
 
 public class ConcatinateAndReverseString {
 	public static void main(String[] args) {
